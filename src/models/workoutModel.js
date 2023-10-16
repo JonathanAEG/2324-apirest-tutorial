39963068ee4bf61a81489076e29f472932cc425c
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2023-10-16 08:08:27 +0200

    [NEW] Creado modelo

---
+++ src/models/workoutModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const workoutSchema = new Schema({
+    name: String,
+    mode: String,
+    equipment: [String]
+});
+
+module.exports = mongoose.model('Workout', workoutSchema);
